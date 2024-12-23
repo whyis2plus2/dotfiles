1c36a1f672d7a7508ccecfdd50b3bb93203a87e7
whyis2plus2 <146612147+whyis2plus2@users.noreply.github.com>
2024-12-22 21:38:47 -0700

    Update init.lua
    switch to dark mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,7 +20,7 @@ Plug("folke/tokyonight.nvim")
 vim.call("plug#end")
 
 -- set the theme
-vim.cmd[[colorscheme tokyonight-day]]
+vim.cmd[[colorscheme tokyonight]]
 
 ---------------------
 ------ KEYBINDS -----
